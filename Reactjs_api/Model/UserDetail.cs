6ab2a20bcf2e7bbc173716fa95baf8f673465920
vinit kumar <vkr82663@gmail.com>
2026-04-09 21:31:47 +0530

    my first project

---
+++ Reactjs_api/Model/UserDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace Reactjs_api.Model
+{
+    public class UserDetail
+    {
+        public int UserId { get; set; }
+        public string UserEmailId { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
